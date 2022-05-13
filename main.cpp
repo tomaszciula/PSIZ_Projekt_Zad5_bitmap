a2b2da1c72bbf3eb38807d4bcb67394401b4a66c
tomaszciula <tomciuns@gmail.com>
2022-05-13 09:41:06 +0200

    bmp file header infos printing

--- main.cpp
+++ main.cpp
@@ -44,6 +44,58 @@ int main(int arc, char * argv[]) {
     {
         printf("\n\n File f opened!");
     }
+        printf("\n INFORMACJE O BITMAPIE\n\n");
+
+    fread(&File.bfType, sizeof(File.bfType), 1, f);
+    printf(" Typ: %x", File.bfType);
+
+    fread(&File.bfSize, sizeof(File.bfSize), 1, f);
+    printf("\n Rozmiar pliku: %d bajtow", File.bfSize);
+
+    fread(&File.bfReserved1, sizeof(File.bfReserved1), 1, f);
+    printf("\n Zarezerwowane1: %d", File.bfReserved1);
+
+    fread(&File.bfReserved2, sizeof(File.bfReserved2), 1, f);
+    printf("\n Zarezerwowane2: %d", File.bfReserved2);
+
+    fread(&File.bfOffBits, sizeof(File.bfOffBits), 1, f);
+    printf("\n Pozycja danych obrazkowych: %d", File.bfOffBits);
+
+    printf("\n");
+
+    fseek(f, 14, SEEK_SET);
+    fread(&Picture.biSize, sizeof(Picture.biSize), 1, f);
+    printf("\n Wielkosc naglowka informacyjnego: %d", Picture.biSize);
+
+    fread(&Picture.biWidth, sizeof(Picture.biWidth), 1, f);
+    printf("\n Szerokosc: %d pikseli", Picture.biWidth);
+
+    fread(&Picture.biHeight, sizeof(Picture.biHeight), 1, f);
+    printf("\n Wysokosc: %d pikseli", Picture.biHeight);
+
+    fread(&Picture.biPlanes, sizeof(Picture.biPlanes), 1, f);
+    printf("\n Liczba platow: %d", Picture.biPlanes);
+
+    fread(&Picture.biBitCount, sizeof(Picture.biBitCount), 1, f);
+    printf("\n Liczba bitow na piksel: %d (1, 4, 8, or 24)", Picture.biBitCount);
+
+    fread(&Picture.biCompression, sizeof(Picture.biCompression), 1, f);
+    printf("\n Kompresja: %d (0=none, 1=RLE-8, 2=RLE-4)", Picture.biCompression);
+
+    fread(&Picture.biSizeImage, sizeof(Picture.biSizeImage), 1, f);
+    printf("\n Rozmiar samego rysunku: %d", Picture.biSizeImage);
+
+    fread(&Picture.biXPelsPerMeter, sizeof(Picture.biXPelsPerMeter), 1, f);
+    printf("\n Rozdzielczosc pozioma: %d", Picture.biXPelsPerMeter);
+
+    fread(&Picture.biYPelsPerMeter, sizeof(Picture.biYPelsPerMeter), 1, f);
+    printf("\n Rozdzielczosc pionowa: %d", Picture.biYPelsPerMeter);
+
+    fread(&Picture.biClrUsed, sizeof(Picture.biClrUsed), 1, f);
+    printf("\n Liczba kolorow w palecie: %d", Picture.biClrUsed);
+
+    fread(&Picture.biClrImportant, sizeof(Picture.biClrImportant), 1, f);
+    printf("\n Wazne kolory w palecie: %d", Picture.biClrImportant);
 
     return 0;
 }
