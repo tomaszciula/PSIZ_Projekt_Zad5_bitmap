5412c3d4bf6052d19100b1a6c879d0f0158fec90
tomaszciula <tomciuns@gmail.com>
2022-05-13 09:16:30 +0200

    adding test.bmp and structs for bmp header

---
+++ main.cpp
@@ -0,0 +1,39 @@
+#pragma once
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+struct FileHeader {
+    short bfType;
+    int bfSize;
+    short bfReserved1;
+    short bfReserved2;
+    int bfOffBits;
+} File;
+
+struct PictureHeader {
+    int biSize;
+    int biWidth;
+    int biHeight;
+    short biPlanes;
+    short biBitCount;
+    int biCompression;
+    int biSizeImage;
+    int biXPelsPerMeter;
+    int biYPelsPerMeter;
+    int biClrUsed;
+    int biClrImportant;
+} Picture;
+
+struct KoloryRGB {
+    char R;
+    char G;
+    char B;
+} Rgb;
+
+int main(int arc, char * argv[]) {
+
+
+
+    return 0;
+}
